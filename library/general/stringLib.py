53cf4a055ca89a038621bb511fa9b1aec78286d3
angiu <an.giusti91@gmail.com>
2025-07-18 18:11:59 +0200

    ADD : library

---
+++ library/general/stringLib.py
@@ -0,0 +1,104 @@
+"""
+===============================================================================
+fileName: stringLib
+scripter: angiu
+creation date: 18/07/2025
+description: 
+===============================================================================
+"""
+# ==== native ==== #
+import sys
+
+# ==== third ==== #
+
+# ==== local ===== #
+
+# ==== global ==== #
+CASES = ['kebab-case', 'snake_case', 'camelCase', 'Train-Case', 'SCREAMING_SNAKE_CASE', 'PascalCase', 'Casual case']
+
+
+def convertStringToCamelCase(string, ):
+    """
+    Convert a string to camelCase.
+
+    :param string: The input string.
+    :type string: str
+
+    :return: The camelCase formatted string.
+    :rtype: str
+    """
+    spaceString = string.replace('_', ' ').replace('-', ' ')
+
+    formatedCase = ''
+    space = False
+    for i, c in enumerate(spaceString):
+        if i == 0:
+            formatedCase += c.upper()
+            continue
+
+        if c == ' ':
+            space = True
+            continue
+
+        if space:
+            formatedCase += c.upper()
+        else:
+            formatedCase += c
+
+        space = False
+
+    return formatedCase
+
+
+def formatString(string, convertAs='Casual case'):
+    """
+    Convert a string to the specified case format.
+
+    :param string: The input string.
+    :type string: str
+    :param convertAs: The target format (e.g., 'kebab-case', 'snake_case', etc.).
+    :type convertAs: str
+
+    :return: The formatted string.
+    :rtype: str
+    """
+    if convertAs not in CASES:
+        print(f'not recognize case: {convertAs}')
+        sys.exit(1)
+
+    camelCased = convertStringToCamelCase(string)
+    if convertAs == 'camelCase':
+        return camelCased
+
+    if convertAs in ['kebab-case', 'Train-Case']:
+        spacer = '-'
+    elif convertAs in ['SCREAMING_SNAKE_CASE', 'snake_case']:
+        spacer = '_'
+    elif convertAs == 'Casual case':
+        spacer = ' '
+    else:
+        spacer = None
+
+    formated = ''
+    for i, c in enumerate(camelCased):
+        if i == 0:
+            if convertAs in ['kebab-case', 'snake_case', 'camelCase']:
+                formated += c.lower()
+            elif convertAs in ['Train-Case', 'SCREAMING_SNAKE_CASE', 'PascalCase', 'Casual case']:
+                formated += c.upper()
+
+            continue
+
+        if c.isupper():
+            if not spacer:
+                formated += c
+            elif convertAs in ['Train-Case', 'SCREAMING_SNAKE_CASE', 'PascalCase']:
+                formated += f'{spacer}{c.upper()}'
+            else:
+                formated += f'{spacer}{c.lower()}'
+
+            continue
+
+        formated += c
+
+    return formated
---
+++ library/general/uiLib.py
@@ -0,0 +1,124 @@
+"""
+===============================================================================
+fileName: uiLib
+scripter: angiu
+creation date: 18/07/2025
+description: 
+===============================================================================
+"""
+# ==== native ==== #
+import os
+import sys
+import importlib
+
+# ==== third ==== #
+from PySide2 import QtWidgets
+
+# ==== local ===== #
+
+# ==== global ==== #
+STYLE_SHEET = {
+    'QPushButton': "background-color: rgb(85, 85, 85); color: rgb(212, 212, 212); border: 4px solid rgb(85, 85, 85)",
+    'QLabel': "color: rgb(212, 212, 212);",
+    'QCheckBox': "color: rgb(212, 212, 212);",
+    'QWidget': "background-color: transparent;",
+    'QTableWidget': "QTableWidget {background-color: rgb(45, 45, 45); color: rgb(212, 212, 212);} QHeaderView::section { background-color: rgb(85, 85, 85); color: rgb(212, 212, 212);}",
+    'QMenuBar': "background-color: transparent; color: rgb(212, 212, 212);",
+    'QLineEdit': "background-color: rgb(85, 85, 85); color: rgb(212, 212, 212); border: 4px solid rgb(85, 85, 85)",
+    'QTextEdit': "background-color: rgb(85, 85, 85); color: rgb(212, 212, 212); border: 4px solid rgb(85, 85, 85)",
+    'QLineEditSpe': "background-color: rgb(45, 45, 45); color: rgb(212, 212, 212); border: 4px solid rgb(45, 45, 45)",
+    'QProgressBar': "background-color: rgb(85, 85, 85); color: rgb(212, 212, 212); border: 4px solid rgb(85, 85, 85); text-align: center;",
+    'QSplitter': 'QSplitter::handle { background-color: rgb(24, 24, 24); width: 1px; height: 1px; }',
+    'QTreeView': 'QHeaderView::section {background-color: rgb(85, 85, 85); color: rgb(212, 212, 212); border: 4px solid rgb(85, 85, 85)} QTreeView {background-color: rgb(85, 85, 85)}'
+    }
+OTHER_WIDGETS = "background-color: rgb(63, 63, 63); color: rgb(212, 212, 212); border: 4px solid rgb(63, 63, 63)"
+
+
+def applyStyleSheet(ui=None, widgets=None, excluded=None):
+    """
+    Apply style sheets to a given UI or list of widgets.
+
+    :param ui: QWidget parent to search widgets from.
+    :type ui: QtWidgets.QWidget or None
+    :param widgets: List of widgets to apply styles to.
+    :type widgets: list[QtWidgets.QWidget] or None
+    :param excluded: Set or list of widgets to exclude.
+    :type excluded: set[QtWidgets.QWidget] or list or None
+    """
+    if not ui and not widgets:
+        return
+
+    if ui:
+        ui.setStyleSheet(f"background-color: rgb(45, 45, 45);")
+
+    if excluded is None:
+        excluded = set()
+
+    widgetData = {}
+    if not widgets:
+        for className in STYLE_SHEET:
+            qtClass = getattr(QtWidgets, className, None)
+            if not qtClass:
+                continue
+
+            for widget in ui.findChildren(qtClass):
+                if widget in excluded:
+                    continue
+
+                widgetType = widget.__class__.__name__
+                widgetData[widget] = STYLE_SHEET.get(widgetType, OTHER_WIDGETS)
+
+    else:
+        for widget in widgets:
+            if widget in excluded:
+                continue
+
+            widgetType = widget.__class__.__name__
+            widgetData[widget] = STYLE_SHEET.get(widgetType, OTHER_WIDGETS)
+
+    for widget, style in widgetData.items():
+        if widget:
+            widget.setStyleSheet(style)
+
+
+def loadUi(mainModule, className, asDialog=False):
+    """load ui module related to given mainModule file path
+
+    :param mainModule: package main module file path
+    :type mainModule: str
+    :param className: name of class to launch
+    :type className: str
+    :param asDialog: if True, loads the UI as a modal QDialog that blocks interaction with other windows until closed
+    :type asDialog: bool
+
+    :return: ui
+    :rtype: module
+    """
+    baseFolder = os.path.split(mainModule)[0]
+    uiFile = os.path.join(baseFolder, 'ui.py')
+
+    spec = importlib.util.spec_from_file_location(f'{className}Ui', uiFile)
+    uiModule = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(uiModule)
+
+
+    toLaunch = getattr(uiModule, f'{className}Ui', None)
+    if toLaunch is None:
+        print(f'not found : {className}Ui')
+        return
+
+    if not QtWidgets.QApplication.instance():
+        app = QtWidgets.QApplication(sys.argv)
+
+    try:
+        if asDialog:
+            uiInstance = toLaunch(parent=None)
+            if isinstance(uiInstance, QtWidgets.QDialog):
+                uiInstance.setModal(True)
+        else:
+            uiInstance = toLaunch()
+    except Exception as e:
+        print(f"Error while instancing : {e}")
+        raise
+
+    return uiInstance
