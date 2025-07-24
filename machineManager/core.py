9a83505b19c80053b0f1d5161f67a75639408466
angiu <an.giusti91@gmail.com>
2025-07-24 08:56:37 +0200

    REFACTO : machinManager core

--- machineManager/core.py
+++ machineManager/core.py
@@ -23,67 +23,70 @@ NEEDED_INFOS = {'secteur': ['1A', '1B', '1C', '2A', '2B', '2C'],
                 'numero de serie': None,
                 "annee d'acquisition": None,
                 'en service': True}
-MACHINE_JSON = os.path.join(BASE_FOLDER, 'data', 'machine.json')
+MACHINE_FOLDER = os.path.join(BASE_FOLDER, 'data', 'machines')
 BUTTONS = ['edit', 'sub', 'add']
 
 
 def addEntry(name, data):
     """
-    Add or update an entry in the MACHINE_JSON file and save the updated data.
+    Add or update a machine entry by saving data to a JSON file in MACHINE_FOLDER.
 
-    :param name: The key under which to store the data.
+    :param name: The machine identifier used as the file name.
     :type name: str
-    :param data: The data to associate with the given key.
+    :param data: The data to store in the machine's JSON file.
     :type data: dict
     """
-    if not os.path.exists(MACHINE_JSON):
-        currentData = {name: data}
+    if not os.path.exists(MACHINE_FOLDER):
+        os.makedirs(MACHINE_FOLDER)
+        print(f'created folder : {MACHINE_FOLDER}')
 
-    else:
-        currentData = getMachineData()
-        currentData[name] = data
+    jsonFile = os.path.join(MACHINE_FOLDER, f'{name}.json')
 
-    saveData(currentData)
+    with open(jsonFile, 'w', encoding='utf-8') as f:
+        json.dump(data, f)
+        print(f'add archive : {jsonFile}')
 
 
 def deleteEntry(name):
     """
-    Delete the specified entry from the MACHINE_JSON file and save the updated data.
+    Delete a machine entry by removing its JSON file from MACHINE_FOLDER.
 
-    :param name: The key of the entry to delete.
+    :param name: The machine identifier whose file will be deleted.
     :type name: str
     """
-    if not os.path.exists(MACHINE_JSON):
-        return
+    jsonFile = os.path.join(MACHINE_FOLDER, f'{name}.json')
 
-    currentData = getMachineData()
-    currentData.pop(name, None)
-
-    saveData(currentData)
+    os.remove(jsonFile)
+    print(f'deleted archive : {jsonFile}')
 
 
-def getMachineData():
+def getMachineData(machineName=None):
     """
-    Load machine data from the MACHINE_JSON file.
-    Checks if MACHINE_JSON exists; returns an empty dict if not.
+    Load machine data from JSON files in MACHINE_FOLDER.
 
-    :return: Parsed machine data as a dict, or empty dict if file is missing.
+    If machineName is provided, only load that machine's data.
+    Returns an empty dict if MACHINE_FOLDER does not exist or no matching file is found.
+
+    :param machineName: The specific machine identifier to load data for, or None to load all machines.
+    :type machineName: str or None
+
+    :return: A dict mapping machine identifiers to their loaded data.
     :rtype: dict
     """
-    if not os.path.exists(MACHINE_JSON):
+    if not os.path.exists(MACHINE_FOLDER):
         return {}
 
-    return json.load(open(MACHINE_JSON, 'r', encoding='utf-8'))
+    data = {}
+    for item in os.listdir(MACHINE_FOLDER):
+        if not item.endswith('.json'):
+            continue
 
+        shortName = os.path.splitext(item)[0]
+        if machineName and shortName != machineName:
+            continue
 
-def saveData(data):
-    """
-    Save the given data to the MACHINE_JSON file in JSON format.
+        itemPath = os.path.join(MACHINE_FOLDER, item)
+        with open(itemPath, 'r', encoding='utf-8') as f:
+            data[shortName] = json.load(f)
 
-    :param data: The data to write to the JSON file.
-    :type data: dict
-    """
-    with open(MACHINE_JSON, 'w', encoding='utf-8') as f:
-        json.dump(data, f)
-
-    print(f'saved as : {MACHINE_JSON}')
+    return data
